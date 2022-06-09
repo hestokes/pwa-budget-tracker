fe1f7ad624c25f04236c801adc2b82236501015c
Aaron Stokes <stokescomp01@gmail.com>
2022-06-09 14:19:13 -0600

    still debugging heroku

--- server.js
+++ server.js
@@ -16,16 +16,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
 });
+
+// process.env.MONGODB_URI || "mongodb://localhost/budget",
+// {
+//   useNewUrlParser: true,
+// useFindAndModify: false,
+// useUnifiedTopology: true
+// });
 
 // routes
 app.use(require("./routes/api.js"));
