70873cf762d77da3090fe0147a3c7724fcf9031a
Aaron Stokes <stokescomp01@gmail.com>
2022-06-09 09:43:22 -0600

    start service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+console.log("checking status");
+
+// resources to build application
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/indexeDB.js",
+  "/styles.css",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+// Prevent caches from using outdated cache
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Successfully pre-cached files");
+      // installs all initial resources/static assets and add to cache
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  // tell browser to activate service worker immediately when it completes installing
+  self.skipWaiting();
+});
+
+// clears cache/reactivates application when complete
+// activate service worker/remove old data from the cache.
+// runs before the install, ensures all version numbers stay the same
+self.addEventListener("activate", function (event) {
+  event.waitUtil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Old cache removed", key);
+            // removes old cache
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  // will not call the server if file is in that cache
+  self.clients.claim();
+});
